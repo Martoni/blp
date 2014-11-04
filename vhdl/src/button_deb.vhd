5fb076ac0cbd0926da1deb05419036970e2771f9
Fabien Marteau <fabien.marteau@armadeus.com>
2014-11-04 13:53:18 +0100

    vhdl/src/button_deb: syntesize without warning

--- vhdl/src/button_deb.vhd
+++ vhdl/src/button_deb.vhd
@@ -22,16 +22,15 @@ Entity button_deb is
 end entity;
 
 Architecture button_deb_1 of button_deb is
-    signal edge : std_logic;
-    signal edge_deb : std_logic;
+    signal edge : std_logic := '0';
     signal debounced : std_logic;
-    signal button_in_s : std_logic;
-    signal button_hold : std_logic;
-    signal button_valid_s : std_logic;
-    CONSTANT HALF_CLK_PER : time := (1 sec) / (clk_freq * 500);
+    signal button_in_s : std_logic := '0';
+    signal button_hold : std_logic := '0';
+    signal button_valid_s : std_logic := '0';
+    CONSTANT CLK_PER : time := (1 sec) / (clk_freq * 1000);
     CONSTANT MAX_COUNT : natural :=
-            (debounce_per_ms / HALF_CLK_PER ) + 1;
-    signal count : natural range 0 to MAX_COUNT;
+            (debounce_per_ms / CLK_PER ) + 1;
+    signal count : natural range 0 to MAX_COUNT := (MAX_COUNT - 1);
 begin
 
     -- synchronize button_in
@@ -49,13 +48,13 @@ begin
 
     -- detecting edge.
     edge_pc: process(clk, rst)
-        variable button_in_edge_old : std_logic;
+        variable button_in_edge_old : std_logic := '0';
     begin
         if (rst = '1') then
-            button_in_edge_old := button_in_s;
+            button_in_edge_old :=  '0';
         elsif rising_edge(clk) then
             edge <= button_in_s xor button_in_edge_old;
-            button_in_edge_old := button_in_s;
+            button_in_edge_old :=  button_in_s;
         end if;
     end process edge_pc;
 
@@ -65,7 +64,7 @@ begin
         if rst = '1' then
             count <= (MAX_COUNT - 1);
         elsif rising_edge(clk) then
-            if count /= (MAX_COUNT - 1) then
+            if count < MAX_COUNT then
                 count <= count + 1;
             elsif edge = '1' then
                 count <= 0;
@@ -74,19 +73,19 @@ begin
     end process count_p;
 
     -- button sig debounced
-    debounced <= edge when count = (MAX_COUNT - 1) else '0';
+    debounced <= edge when count = MAX_COUNT else '0';
 
     -- button commute
     commute_p : process(clk, rst)
     begin
         if rst = '1' then
             button_hold <= '0';
-            button_valid_s <= button_in_s;
+            button_valid_s <= '0';
         elsif rising_edge(clk) then
             if (debounced = '1') and (edge = '1') then
-                button_hold <= not button_hold;
+                button_hold <=  not button_hold;
             else
-                button_hold <= button_hold;
+                button_hold <=  button_hold;
             end if;
             if debounced = '1' and button_hold = '0' then
                 button_valid_s <= not button_valid_s;
