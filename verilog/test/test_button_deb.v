2b1f4e1a055bd4e97edbd53987ba78e8cea8d446
Fabien Marteau <fabien.marteau@armadeus.com>
2014-11-14 08:56:24 +0100

    verilog: stimuly process

--- verilog/test/test_button_deb.v
+++ verilog/test/test_button_deb.v
@@ -2,6 +2,8 @@
 
 module test_button_deb;
 
+	parameter DEBOUNCE_PER_MS = 20;
+
   /* Make a reset that pulses once. */
   reg rst = 1;
   initial begin
@@ -11,17 +13,29 @@ module test_button_deb;
 
   /* Make a regular pulsing clock. */
   reg clk = 0;
+
   always #0.526 clk = !clk;
 
-  wire button_in;
+  reg button_in;
   wire button_valid;
   button_deb button(clk, rst, button_in, button_valid);
 
   initial begin
     $dumpfile("simu/button_deb_tb.vcd");
     $dumpvars(0, clk, rst, button_in);
+	end
+
+  initial begin
     $monitor("At time %t, value = %h (%0d)",
               $time, button_in, button_in);
+	end
+
+	initial begin
+		assign button_in = 1;
+		# 1
+		assign button_in = 0;
+		# 10
+		assign button_in = 1;
     # 500 $finish;
   end
 
