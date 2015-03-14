dc99b939a1d488bf64397ab7ddb4db10363f5ef2
Fabien Marteau <fabien.marteau@armadeus.com>
2015-03-14 18:31:25 +0100

    verilog: change interface presentation for debounce

--- verilog/src/button_deb.v
+++ verilog/src/button_deb.v
@@ -1,18 +1,17 @@
 // Author : Fabien Marteau <fabien.marteau@armadeus.com>
 // Creation Date : 02/11/2014
 
-module button_deb(clk, rst, button_in, button_valid);
+module button_deb(
+        // sync design
+        input    clk,
+        input    rst,
+        // in-out
+        input    button_in,
+        output   button_valid);
 
-//Generics parameters
+    //Generics parameters
 parameter clk_freq = 95000; // clock frequency in kHz
 parameter debounce_per_ms = 20;
-
-// sync design
-input    clk;
-input    rst;
-// in-out
-input    button_in;
-output   button_valid;
 
 //Architecture 
 
