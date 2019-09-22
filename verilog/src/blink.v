2c4445e6403dd974c814dd2741ee111c7f78b353
Martoni <mail@fabienm.eu>
2019-09-22 15:32:45 +0200

    Update blink.v
    synthesizeable code

--- verilog/src/blink.v
+++ verilog/src/blink.v
@@ -1,7 +1,5 @@
 // Author: Fabien Marteau <mail@fabienm.eu
 // Creation Date : 14/09/2019
-
-
 module blink (
     // Horloge
     input clock,
@@ -9,25 +7,29 @@ module blink (
 );
 
 // Icestick clock : 12Mhz
-//parameter clock_freq = 12_000_000 // clock frequency
+//parameter clock_freq = 12_000_000; // clock frequency
 // LicheeTang clock : 24Mhz
-parameter clock_freq = 24_000_000 // clock frequency
+parameter clock_freq = 24_000_000; // clock frequency
 
 localparam MAX_COUNT = clock_freq;
-localparam MAX_COUNT_UPPER = $clog2(MAX_COUNT) - 1;
+localparam MAX_COUNT_UPPER = $clog2(MAX_COUNT) - 1;
 
 reg [MAX_COUNT_UPPER:0] counter;
+reg led_reg;
 
-    always@(posedge clock)
-    begin
-        if(count < MAX_COUNT/2)
-            led <= 1;
-        else
-            led <= 0;
-        if(count >= MAX_COUNT)
-            count <= 0;
-        else
-            count <= count + 1;
-    end
+assign led = led_reg;
+
+always@(posedge clock)
+begin
+    if(counter < MAX_COUNT/2)
+        led_reg <= 1;
+    else
+        led_reg <= 0;
+
+    if(counter >= MAX_COUNT)
+        counter <= 0;
+    else
+        counter <= counter + 1;
+end
 
 endmodule
